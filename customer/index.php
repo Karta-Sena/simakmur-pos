d63ac39d7f002d20749d15a49a50bbd2611e60f8
Karta Sena Wisesa <arpomuse@gmail.com>
2025-12-01 10:39:45 +0700

    penambahan variabel font baru Laraboyok

--- customer/index.php
+++ customer/index.php
@@ -11,9 +11,9 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,400&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/fonts.css">
     <link rel="stylesheet" href="css/customer.css">
 </head>
 
@@ -47,8 +47,7 @@
             <div class="divider-wavy-charcoal" aria-hidden="true"></div>
 
             <div class="hero-headline-split">
-                <h1 class="hero-text serif">Hallo,</h1>
-                <h1 class="hero-text serif font-black accent-underline">Selamat Datang di Kedai Sekar Makmur</h1>
+                <h1 class="hero-text serif font-black accent-underline">Sugeng Rawuh di Kedai Sekar Makmur</h1>
             </div>
         </header>
 
